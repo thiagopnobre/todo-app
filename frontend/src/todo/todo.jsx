ed780c03a086aa0931fb64c8354f5e2f3bfbb86a
thiagopnobre <thiago_pnobre@hotmail.com>
2018-07-13 11:43:51 -0300

    Handle add

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 
+const BASE_URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { description: '', list: [] }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ ...this.state, description: event.target.value })
+  }
+
+  handleAdd() {
+    const description = this.state.description
+    axios.post(BASE_URL, { description })
+  }
+
   render() {
     return (
       <div>
         <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-        <TodoForm />
+        <TodoForm description={this.state.description}
+                  handleChange={this.handleChange}
+                  handleAdd={this.handleAdd} />
       </div>
     )
   }
